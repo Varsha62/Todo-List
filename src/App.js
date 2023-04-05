ef65af1731ab7b2e718bce6496dd980e85159774
Varsha62 <vinsisahu3003@gmail.com>
2023-04-05 18:31:15 +0530

    we are store local storage our todolist

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React, { useState } from 'react'
-import "./App.css"
+import React, { useState, useEffect } from 'react';
+import "./App.css";
 import TodoInput from './Components/TodoInput';
 import TodoList from './Components/TodoList';
+
 function App() {
-  const [listTodo,setListTodo]=useState([]);
-  //function call kr rhe hai 
+  const [listTodo, setListTodo] = useState(() => {
+    // Check if data is present in local storage
+    const data = localStorage.getItem("listTodo");
+    if (data) {
+      return JSON.parse(data);
+    } else {
+      return [];
+    }
+  });
+
+  // Update local storage when listTodo state changes
+  useEffect(() => {
+    localStorage.setItem("listTodo", JSON.stringify(listTodo));
+  }, [listTodo]);
+
   let addList = (inputText)=>{
     if(inputText!=='')
-      // array type call krne ke liye
       setListTodo([...listTodo,inputText]);
   }
+
   const deleteListItem = (key)=>{
     let newListTodo = [...listTodo];
     newListTodo.splice(key,1)
     setListTodo([...newListTodo])
   }
+
   return (
     <div className="main-container">
       <div className="center-container">
@@ -28,8 +43,8 @@ function App() {
         })}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
